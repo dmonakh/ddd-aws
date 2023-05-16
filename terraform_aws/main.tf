ea532874762bb71c1fb6cc1ed5d0a2dbc5d7e451
dmonakh <d.monakh@it-sprout.org>
2023-05-16 21:00:55 +0300

    aws

---
+++ terraform_aws/main.tf
@@ -0,0 +1,183 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+data "aws_availability_zones" "available" {}
+
+locals {
+  cluster_name = "mondyk8awsklas"
+}
+
+module "eks-kubeconfig" {
+  source     = "hyperbadger/eks-kubeconfig/aws"
+  version    = "1.0.0"
+
+  depends_on = [module.eks]
+  cluster_id =  module.eks.cluster_name
+  }
+
+resource "local_file" "kubeconfig" {
+  content  = module.eks-kubeconfig.kubeconfig
+  filename = "kubeconfig_${local.cluster_name}"
+}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "3.18.1"
+
+  name                 = "k8s-vpc"
+  cidr                 = "172.16.0.0/16"
+  azs                  = data.aws_availability_zones.available.names
+  private_subnets      = ["172.16.1.0/24", "172.16.2.0/24", "172.16.3.0/24"]
+  public_subnets       = ["172.16.4.0/24", "172.16.5.0/24", "172.16.6.0/24"]
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
+  enable_dns_hostnames = true
+
+  public_subnet_tags = {
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+    "kubernetes.io/role/elb"                      = "1"
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+    "kubernetes.io/role/internal-elb"             = "1"
+  }
+}
+
+module "eks" {
+  source = "terraform-aws-modules/eks/aws"
+  version = "19.5.1"
+
+  cluster_name    = "${local.cluster_name}"
+  cluster_version = "1.24"
+  subnet_ids      = module.vpc.private_subnets
+
+  cluster_endpoint_private_access = true
+  cluster_endpoint_public_access = true
+
+  vpc_id = module.vpc.vpc_id
+
+  enable_irsa = true
+}
+
+resource "aws_iam_role" "nodes" {
+  name = "eks-node-group-nodes"
+
+  assume_role_policy = jsonencode({
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+    }]
+    Version = "2012-10-17"
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "AmazonEKSWorkerNodePolicy" {
+  role       = aws_iam_role.nodes.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy" 
+}
+
+resource "aws_iam_role_policy_attachment" "AmazonEC2ContainerRegistryReadOnly" {
+  role       = aws_iam_role.nodes.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
+
+resource "aws_launch_template" "eks_node_group_template" {
+  name_prefix   = "${local.cluster_name}-"
+  instance_type = "t2.micro"
+
+  image_id = "ami-0889a44b331db0194"
+
+  block_device_mappings {
+    device_name = "/dev/xvda"
+
+    ebs {
+      volume_size = 20
+      delete_on_termination = true
+      volume_type = "gp2"
+    }
+  }
+
+  tag_specifications {
+    resource_type = "instance"
+
+    tags = {
+      Name = "${local.cluster_name}-worker-node"
+    }
+  }
+}
+
+resource "aws_lb_target_group" "eks_target_group" {
+  name     = "eks-target-group"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = module.vpc.vpc_id
+
+  health_check {
+    path                = "/"
+    port                = "traffic-port"
+    protocol            = "HTTP"
+    interval            = 30
+    timeout             = 10
+    healthy_threshold   = 3
+    unhealthy_threshold = 3
+  }
+}
+
+resource "aws_autoscaling_group" "eks_node_group" {
+  name                      = "${local.cluster_name}-nodes"
+  min_size                  = 1
+  max_size                  = 2
+  desired_capacity          = 1
+  launch_template {
+    id      = aws_launch_template.eks_node_group_template.id
+    version = "$Latest"
+  }
+
+  vpc_zone_identifier       = module.vpc.private_subnets
+  tag {
+    key                 = "kubernetes.io/cluster/${local.cluster_name}"
+    value               = "true"
+    propagate_at_launch = true
+  }
+
+  tag {
+    key                 = "k8s.io/role/node"
+    value               = "1"
+    propagate_at_launch = true
+  }
+}
+
+resource "aws_autoscaling_attachment" "eks_node_group_attachment" {
+  autoscaling_group_name = aws_autoscaling_group.eks_node_group.name
+  lb_target_group_arn    = aws_lb_target_group.eks_target_group.arn
+}
+
+# resource "aws_eks_node_group" "private-nodes" {
+#   cluster_name    = module.eks.cluster_name
+#   node_group_name = "${local.cluster_name}-nodes"
+#   node_role_arn   = aws_iam_role.nodes.arn
+
+#   subnet_ids =  module.vpc.private_subnets
+
+#   scaling_config {
+#     desired_size = 1
+#     max_size     = 2
+#     min_size     = 0
+#   }
+
+#   launch_template {
+#     id      = aws_launch_template.eks_node_group_template.id
+#     version = "$Latest"
+#   }
+
+#   depends_on = [
+#     aws_iam_role_policy_attachment.AmazonEKSWorkerNodePolicy,
+#     aws_iam_role_policy_attachment.AmazonEC2ContainerRegistryReadOnly,
+#     aws_autoscaling_attachment.eks_node_group_attachment,
+#   ]
+# }
