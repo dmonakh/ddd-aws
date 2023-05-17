9bf322fe09b8ea5d6c601bf08040023160196fc5
dmonakh <d.monakh@it-sprout.org>
2023-05-17 17:12:32 +0300

    test

--- terraform_aws/backend.tf
+++ terraform_aws/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "bucket_name=mondytestbucket183"  
+    bucket         = "mondytestbucket183"  
     key            = "terraform.tfstate"   
     region         = "us-east-1"           
     encrypt        = true                  
